00d4a5fadf245d53ea312f62e88c2935b7a86ddb
Shahzeb Akhtar <shahzebakhtar66@gmail.com>
2024-11-20 13:56:23 +0530

    removed github

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,10 +12,6 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_ID || "",
       clientSecret: process.env.GOOGLE_SECRET || "",
     }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
-    }),
   ],
 
   secret: process.env.NEXTAUTH_SECRET,
